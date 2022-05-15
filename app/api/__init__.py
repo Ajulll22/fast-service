64734cb38ee27a2faaae74794cfb30b21b73be01
Ajulll22 <ajulrizki@gmail.com>
2022-05-15 14:18:04 +0700

    get profile

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,6 +4,7 @@ from app.api.auth.auth_register import auth_register
 from app.api.auth.auth_login import auth_login, LoginResponseModel
 from app.api.auth.auth_logout import auth_logout
 from app.api.auth.auth_refresh_token import auth_refresh_token, RefreshTokenResponseModel
+from app.api.auth.get_profile import get_profile, GetProfileResponseModel
 
 
 api_router = APIRouter()
@@ -16,3 +17,5 @@ api_router.add_api_route('/api/v1/auth/logout', auth_logout,
                          methods=['POST'], tags=['Auth'], status_code=204)
 api_router.add_api_route('/api/v1/auth/refresh-token', auth_refresh_token,
                          methods=['POST'], tags=['Auth'], response_model=RefreshTokenResponseModel)
+api_router.add_api_route('/api/v1/auth/profile', get_profile,
+                         methods=['GET'], tags=['Auth'], response_model=GetProfileResponseModel)
--- app/api/auth/auth_refresh_token.py
+++ app/api/auth/auth_refresh_token.py
@@ -44,15 +44,25 @@ async def auth_refresh_token(data: RefreshTokenData, session=Depends(get_db_sess
         sa.select(
             UserLogin.id,
             User.id.label('user_id'),
-            User.username
+            User.username,
+            sa.func.if_(
+                UserLogin.expired_at > sa.func.NOW(), 0, 1
+            ).label(
+                'expired'
+            )
         ).where(
             UserLogin.user_id == User.id,
-            UserLogin.refresh_token == data.refresh_token
-        )
+            UserLogin.refresh_token == data.refresh_token)
     ).fetchone()
 
     if not user_login:
         raise HTTPException(400, detail='Refresh token not found')
+
+    if user_login.expired:
+        raise HTTPException(403, detail={
+            'message': 'Refresh token expired',
+            'code': 40301
+        })
 
     # extend exp refresh token
     session.execute(
